8eda4f996779b8976460d19328a4b7e358ac572d
RahulReddyNannuru <rnannuru@gmail.com>
2016-11-16 01:10:49 -0500

    Declared required variables!!

--- app/src/main/java/com/example/rnann/androidapp_areacalculator_radiobuttons/MainActivity.java
+++ app/src/main/java/com/example/rnann/androidapp_areacalculator_radiobuttons/MainActivity.java
@@ -2,12 +2,43 @@ package com.example.rnann.androidapp_areacalculator_radiobuttons;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    // Declaring the view variables globally -> to make life easier
+
+    EditText lengthOneValueEditText;
+    EditText lengthTwoValueEditText;
+
+    TextView resultValueTextView;
+
+    RadioButton triangle;
+    RadioButton square;
+    RadioButton circle;
+    RadioButton rectangle;
+    RadioButton clear;
+
+    Button calculate;
+
+    double  length1;
+    double  length2;
+
+    double area;
+
+    String result;
+
+    // Done With Declaration!!
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
     }
 }
