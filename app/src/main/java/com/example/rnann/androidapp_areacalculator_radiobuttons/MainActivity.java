5c783d50c479fd8aa71d88bd0cb83ca935c5607c
RahulReddyNannuru <rnannuru@gmail.com>
2016-11-16 01:30:59 -0500

    Done with Triangle Logic!!

--- app/src/main/java/com/example/rnann/androidapp_areacalculator_radiobuttons/MainActivity.java
+++ app/src/main/java/com/example/rnann/androidapp_areacalculator_radiobuttons/MainActivity.java
@@ -2,11 +2,13 @@ package com.example.rnann.androidapp_areacalculator_radiobuttons;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -65,7 +67,47 @@ public class MainActivity extends AppCompatActivity {
         rg.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(RadioGroup group, int checkedId) {
-                
+
+
+                switch (checkedId){
+
+                    case R.id.TriangleRadioButton:
+                    {
+                        if (lengthOneValueEditText.getText().toString().equals(null)||lengthOneValueEditText.getText().toString().equals("")||lengthTwoValueEditText.getText().toString().equals(null)||lengthTwoValueEditText.getText().toString().equals("")){
+                            Toast.makeText(MainActivity.this,"Please Enter values in  Length1 and Length2 respectively!!", Toast.LENGTH_SHORT).show();
+                        }else {
+
+                            length1 = Double.parseDouble(lengthOneValueEditText.getText().toString());
+                            length2 = Double.parseDouble(lengthTwoValueEditText.getText().toString());
+
+                            //Done reading Input values entered by the User!!
+
+
+                            //printing the values read to logcat to check functioning of the App
+
+                            Log.d("rahul", "length1 is :" + length1);
+                            Log.d("rahul", "length2 is :" + length2);
+
+                            // Done checking, Values got printed  in the LogCat!!
+
+                            //Calculating the area
+                            area = 0.5 * length1 * length2;
+                            Log.d("rahul", "triangle area is :" + area);
+
+
+
+                            //converting area from double to string -> to display the result as text on the result Text View
+                            result = Double.toString(area);
+
+
+                            resultValueTextView.setText(result);
+
+                            //Calculated area and printed it on the Area Result Text View!!
+
+                        }
+                    }
+                }
+
             }
         });
 
